ce4148a8b6bb83da0dffe8a0bd0b5a050a15bd1a
DESKTOP-SKMPL8O\Legend Hadi <mohammad.hadi.hosseini81@gmail.com>
2024-12-16 16:27:42 +0330

    Complete checkbox widget

--- lib/widgets.dart
+++ lib/widgets.dart
@@ -16,19 +16,53 @@ class TasksList extends StatelessWidget {
   }
 }
 
-class TaskTile extends StatelessWidget {
-  const TaskTile({
-    super.key,
-  });
+class TaskTile extends StatefulWidget {
+  const TaskTile({super.key});
+
+  @override
+  State<TaskTile> createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+  void checkboxCallback() {
+    setState(() {
+      isChecked = !isChecked;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: const Text('This is a task'),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (value) {},
+      title: Text(
+        'This is a task',
+        style: TextStyle(
+          decoration: isChecked ? TextDecoration.lineThrough: null,
+        ),
       ),
+      trailing: TaskCheckbox(
+          checkboxState: isChecked, toggleCheckboxState: checkboxCallback,),
+    );
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+  final bool checkboxState;
+  final Function toggleCheckboxState;
+  const TaskCheckbox({
+    super.key,
+    required this.checkboxState,
+    required this.toggleCheckboxState,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value: checkboxState,
+      onChanged: (value) {
+        toggleCheckboxState();
+      },
     );
   }
 }
